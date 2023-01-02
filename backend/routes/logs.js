337bf1466228a61ced270f241d18321f498445fc
Billy Rukanga <billyrukanga.br@gmail.com>
2023-01-02 13:28:34 +0300

    updated api

--- backend/routes/logs.js
+++ backend/routes/logs.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const device = require("../controllers/devices/device.controller.js")
 const validateUser = require("../middleware/validatejwt");
 
-router.post("/logs", device.createLog);
+router.post("/", device.createLog);
 
 router.get("/:id", validateUser, device.getLogs);
 
