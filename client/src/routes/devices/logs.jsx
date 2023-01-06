6418fc5466a8facfc1ac1e371dd7fdc828475aaa
Billy Rukanga <billyrukanga.br@gmail.com>
2023-01-06 14:37:08 +0300

    updated time

--- client/src/routes/devices/logs.jsx
+++ client/src/routes/devices/logs.jsx
@@ -3,19 +3,31 @@ import { DataGrid } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
 import { getLogs } from "../../api/device_logs";
 
+function formatAMPM(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  const ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  return `${hours}:${minutes}:${seconds} ${ampm}`;
+}
+
 const columns = [
   { field: "id", headerName: "ID", width: 90 },
   {
     field: "date",
     headerName: "Date",
     width: 150,
-    valueGetter: (params) => `${new Date(params.row.timestamp).toLocaleDateString()}`,
+    valueGetter: (params) => `${new Date(params.row.timestamp).toDateString()}`,
   },
   {
     field: "time",
     headerName: "Time",
     width: 150,
-    valueGetter: (params) => `${new Date(params.row.timestamp).toLocaleTimeString()}`,
+    valueGetter: (params) => `${formatAMPM(new Date(params.row.timestamp))}`,
   },
   {
     field: "weight",
